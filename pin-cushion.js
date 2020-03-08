3c14f06db1aa880eded055ed395bf43b8ee368f4
eclarke12 <42503461+eclarke12@users.noreply.github.com>
2020-03-07 20:57:28 -0800

    v010 -Removed img preview

--- pin-cushion.js
+++ pin-cushion.js
@@ -9,24 +9,11 @@ Hooks.on("renderNoteConfig", (app, html, data) => {
         <button type="button" name="file-picker" class="file-picker" data-type="image" data-target="icon" title="Browse Files" tabindex="-1">
         <i class="fas fa-file-import fa-fw"></i>
         </button>`
-    
-    const iconPreviewHtml = 
-        `<img name="icon" src="${data.object.icon}" width="40" style="background: rgba(0, 0, 0, 0.5)">`
 
     const iconSelector = html.find("select[name='icon']");
-    const formGroup = iconSelector.closest("div.form-group");
 
     iconSelector.replaceWith(filePickerHtml);
-    formGroup.before(iconPreviewHtml);
 
     // Detect and activate file-picker buttons
     html.find('button.file-picker').each((i, button) => app._activateFilePicker(button));
-    
-    html.find('input.icon-path').on("change", event => {
-        const iconPath = event.currentTarget.value;
-
-        const iconPreview = html.find("img[name='icon']");
-
-        iconPreview.attr("src", iconPath);
-    });
 });
